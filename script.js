b8bccc155d36f2fc612c0d9cc4466b548a3ec51e
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-07-20 20:39:59 +0300

    Exponent calculator

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // calculation of exponentiation (get the number and power from the user)
 // compute all possible options with a for loop
-// using one for loop
+// using one "for" loop
 // degree can be positive, negative, or 0
 // a^b = a*a b times;
 // a^-b= 1/a^b;
@@ -9,21 +9,18 @@
 const base = parseFloat(prompt("Enter number:")); // the function parseFloat returns a floating-point number
 const exponent = parseInt(prompt("Enter Exponentiation:")); // the function parseInt returns an integer
 
-if (isNaN(base) || isNaN(exponent)) { // the function isNaN(value) checking value for being NaN
-	alert("You entered uncorrect value. please, try again.") //if true, write a message
-} else {
+if (isNaN(base) || isNaN(exponent)) // the function isNaN(value) checking value for being NaN
+	alert("You entered uncorrect value. please, try again.") //if isNaN(value) is true, a message will be written
+else {
 	let result = 1;
 
 	for (let i = 0; i < exponent || i < -exponent; i++) {
-		if (exponent > 0) {
+		if (exponent > 0)
 			result *= base; // a^b = a*a b times - multiplication of a number by itself a specified number of times
-		} else if (exponent < 0) {
+		else if (exponent < 0)
 			result /= base; // a^-b= 1/a^b - negative degree, if the exponent has a negative value
-		} else {
+		else
 			result = 1; // a^0 = 1 - any number to the zero power is always 1
-		}
 	}
-	console.log(result);
-	alert(`${base} ^ ${exponent} = ${result}`);
 	document.write(`<h2>${base} ^ ${exponent} = ${result}</h2>`);
 }
